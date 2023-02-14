48372f1dfa2134cbdeda2f44be08ac6245f63628
suke <suke083.tech@gmail.com>
2023-02-14 23:44:41 +0900

    pass 4th stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use anyhow::{Context, Result};
+use std::env;
 use std::io::{self, Write};
+use std::os::unix::fs;
+use std::path::Path;
 use std::process::{Command, Stdio};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -7,6 +10,13 @@ fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
+    let dir = env::temp_dir();
+    copy_executable_in_directory(&Path::new(command), dir.as_path())?;
+    create_dev_null(&dir)?;
+
+    fs::chroot(&dir)?;
+    std::env::set_current_dir("/")?;
+
     let output = Command::new(command)
         .stderr(Stdio::inherit())
         .args(command_args)
@@ -22,7 +32,12 @@ fn main() -> Result<()> {
         io::stdout().write_all(&output.stdout).unwrap();
     } else {
         match output.status.code() {
-            Some(code) => std::process::exit(code),
+            Some(code) => {
+                let stdout = io::stdout();
+                let mut handle = stdout.lock();
+                handle.write_all(&output.stdout)?;
+                std::process::exit(code)
+            }
             None => {
                 let stderr = io::stderr();
                 let mut handle = stderr.lock();
@@ -31,5 +46,20 @@ fn main() -> Result<()> {
         }
     }
 
+    Ok(())
+}
+
+fn copy_executable_in_directory(executable_path: &Path, chroot_dir: &Path) -> Result<()> {
+    let striped_prefix_path = executable_path.strip_prefix("/")?;
+    let executable_path_inside_dir = chroot_dir.join(striped_prefix_path);
+
+    std::fs::create_dir_all(&executable_path_inside_dir.parent().unwrap())?;
+    std::fs::copy(executable_path, &executable_path_inside_dir)?;
+    Ok(())
+}
+
+fn create_dev_null(chroot_dir: &Path) -> Result<()> {
+    std::fs::create_dir_all(chroot_dir.join("dev"))?;
+    std::fs::File::create(chroot_dir.join("dev/null"))?;
     Ok(())
 }
